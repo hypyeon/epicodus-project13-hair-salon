c2c3f67ea33486d7c0c906dc87574737c0c17fd1
Hayeong Pyeon <hayeong.pyeon@gmail.com>
2024-03-08 23:21:45 -0800

    add settings + trying to fix bug for Stylist Index

--- HairSalon/Controllers/StylistsController.cs
+++ HairSalon/Controllers/StylistsController.cs
@@ -1,6 +1,8 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Mvc;
 using HairSalon.Models;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace HairSalon.Controllers
 {
